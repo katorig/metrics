fd4b13c2a068c5769620db5c886a431f364f09d0
ekaterina.gruzdova <ekaterina.gruzdova@tele2.ru>
2021-06-21 13:57:47 +0300

    edit test_utils

--- app/src/tests/test_utils.py
+++ app/src/tests/test_utils.py
@@ -11,15 +11,17 @@ class TestMetrics(unittest.TestCase):
         super().tearDown()
 
     def test_send_email(self):
-        kwargs = {"send_to": send_to, "subject": "TEST", "text": "test"}
-        self.assertEqual(send_email(kwargs), 'Notification was sent')
+        settings.M_SEND_TO = "ekaterina.gruzdova@tele2.ru"
+        self.assertEqual(send_email(text='TEST'), 'Notification was sent')
 
     def test_send_email_to_many_people(self):
-        kwargs = {"send_to": "ekaterina.gruzdova@tele2.ru, andrey.a.bogomolov@tele2.ru",
-                  "subject": "TEST", "text": "test"}
-        self.assertEqual(send_email(kwargs), 'Notification was sent')
+        settings.M_SEND_TO = "ekaterina.gruzdova@tele2.ru, andrey.a.bogomolov@tele2.ru"
+        self.assertEqual(send_email(text='TEST'), 'Notification was sent')
 
     def test_notification(self):
+        settings.M_SEND_TO = "ekaterina.gruzdova@tele2.ru"
+        settings.M_MODEL_ID = 'test'
+
         @notification
         def foo(flag, notify=0):
             print('Function foo is working')
