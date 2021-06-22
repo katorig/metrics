2bb91c1dc5a8b715c51614e315404bae883217b5
ekaterina.gruzdova <ekaterina.gruzdova@tele2.ru>
2021-06-22 19:03:39 +0300

    rewrite export of variables to flags assignment

--- app/src/main/tele2_metrics.py
+++ app/src/main/tele2_metrics.py
@@ -1,54 +1,82 @@
 #!/usr/bin/env python3
 
-from metrics.metrics import Metrics
 from argparse import ArgumentParser
 from utils.logs_maker import init_logger
+from dynaconf import settings
 import sys
 
 logger = init_logger(__name__)
 
 
-def define_main_args_parser(methods_list: list) -> ArgumentParser:
+def define_main_args_parser() -> ArgumentParser:
     parser = ArgumentParser()
     parser.add_argument("method",
                         nargs="?",
                         help=f"""
-Don't forget to assign environment variables: 
-DYNACONF_METRICS_MODEL_ID (int)
-DYNACONF_METRICS_STAGE_DB (teradata or hadoop)
-DYNACONF_METRICS_STAGE_TABLE (name of stage table in format schema.table)
-DYNACONF_METRICS_FINAL_DB (teradata or hadoop)
-DYNACONF_METRICS_FINAL_TABLE (name of final scoring table in format schema.table)
-DYNACONF_METRICS_REPORT_DATE (report_date in format yyyy-mm-dd)
-DYNACONF_METRICS_RETRO_DATE (previous or retro date in format yyyy-mm-dd)
-DYNACONF_METRICS_THRESHOLD (acceptable limit in percentage deviation between report_date scoring and retro_date scoring)
-DYNACONF_METRICS_COLUMN (column for searching for duplicates)
-DYNACONF_METRICS_SEND_TO (emails for notification, i.e. 'y@a.ru, i@a.ru')
-DYNACONF_METRICS_RETRO_TYPE (type 'retro_df' or 'retro_df_many_dates' depends on with which retro dataframe you want to compare actual dataframe)
 Type name of method you want to run.
-Possible methods: {', '.join(methods_list[1:])}.
+Possible methods: "no_function", "check_df_for_duplicates", 
+"compare_new_df_with_retro", "check_if_data_in_table".
 Default method: no_function.
 If you want to send notification add --notify parameter.""",
-                        choices=methods_list,
+                        choices=["no_function", "check_df_for_duplicates",
+"compare_new_df_with_retro", "check_if_data_in_table"],
                         default='no_function',
                         )
+    parser.add_argument('--model_id', metavar='model_id', type=int, default=None,
+                        help="ID of model, integer")
+    parser.add_argument('--stage_db', metavar='stage_db', type=str, default=None,
+                        help="teradata or hadoop")
+    parser.add_argument('--stage_table', metavar='stage_table', type=str, default=None,
+                        help="name of stage table in format schema.table")
+    parser.add_argument('--final_db', metavar='final_db', type=str, default=None,
+                        help="teradata or hadoop")
+    parser.add_argument('--final_table', metavar='final_table', type=str, default=None,
+                        help="name of final scoring table in format schema.table")
+    parser.add_argument('--report_date', metavar='report_date', type=str, default=None,
+                        help="report_date in format yyyy-mm-dd")
+    parser.add_argument('--retro_date', metavar='retro_date', type=str, default=None,
+                        help="retro date in format yyyy-mm-dd")
+    parser.add_argument('--threshold', metavar='threshold', type=int, default=None,
+                        help="""acceptable limit in percentage deviation 
+                        between report_date scoring and retro_date scoring""")
+    parser.add_argument('--column', metavar='column', type=str, default=None,
+                        help="column for searching for duplicates")
+    parser.add_argument('--send_to', metavar='send_to', type=str, default=None,
+                        help="emails for notification, i.e. 'y@a.ru, i@a.ru'")
+    parser.add_argument('--retro_type', metavar='retro_type', type=str, default=None,
+                        help="""
+                        type 'retro_df' or 'retro_df_many_dates' depends on 
+                        with which retro dataframe you want to compare actual dataframe""")
     parser.add_argument('--notify', metavar='notify', type=int, default=None,
-                        help="type 1 for notification by email or 0 for no notification")
+                        help="1 is for notification by email or 0 is for no notification")
     return parser
 
 
 def foo(*args):
-    logger.error(f"Error! Choose method to run! Possible methods: {', '.join(methods_list[1:])}")
+    logger.error(f"""Error! Choose method to run! 
+Possible methods: "no_function", "check_df_for_duplicates", 
+"compare_new_df_with_retro", "check_if_data_in_table" """)
     sys.exit(1)
 
 
 if __name__ == '__main__':
+    parser = define_main_args_parser()
+    args, sub_args = parser.parse_known_args()
+    settings.METRICS_MODEL_ID = args.model_id
+    settings.METRICS_STAGE_DB = args.stage_db
+    settings.METRICS_STAGE_TABLE = args.stage_table
+    settings.METRICS_FINAL_DB = args.final_db
+    settings.METRICS_FINAL_TABLE = args.final_table
+    settings.METRICS_REPORT_DATE = args.report_date
+    settings.METRICS_RETRO_DATE = args.retro_date
+    settings.METRICS_THRESHOLD = args.threshold
+    settings.METRICS_COLUMN = args.column
+    settings.METRICS_SEND_TO = args.send_to
+    settings.METRICS_RETRO_TYPE = args.retro_type
+    from metrics.metrics import Metrics
     mtr = Metrics()
     methods_dict = {"no_function": foo,
                     "check_df_for_duplicates": mtr.check_df_for_duplicates,
                     "compare_new_df_with_retro": mtr.compare_new_df_with_retro,
                     "check_if_data_in_table": mtr.check_if_data_in_table}
-    methods_list = list(methods_dict.keys())
-    parser = define_main_args_parser(methods_list)
-    args, sub_args = parser.parse_known_args()
     methods_dict.get(args.method)(args.notify)
--- app/src/tests/test_run.sh
+++ app/src/tests/test_run.sh
@@ -2,20 +2,19 @@
 
 kinit -kt ../../../secrets/ekaterina.gruzdova.keytab ekaterina.gruzdova@CORP.TELE2.RU
 
-export DYNACONF_METRICS_MODEL_ID=135
-export DYNACONF_METRICS_STAGE_DB='hadoop'
-export DYNACONF_METRICS_STAGE_TABLE='developers.eg_msg_traf_1'
-export DYNACONF_METRICS_FINAL_DB='teradata'
-export DYNACONF_METRICS_FINAL_TABLE='prd2_dds_v.scoring'
-export DYNACONF_METRICS_REPORT_DATE='2021-06-04'
-export DYNACONF_METRICS_RETRO_DATE='2021-03-04'
-export DYNACONF_METRICS_THRESHOLD=15
-export DYNACONF_METRICS_COLUMN='subs_id'
-export DYNACONF_METRICS_SEND_TO='ekaterina.gruzdova@tele2.ru'
-export DYNACONF_METRICS_RETRO_TYPE='retro_df_many_dates'
-
 cd ../main
 chmod +x tele2_metrics.py
-./tele2_metrics.py compare_new_df_with_retro --notify 1
-./tele2_metrics.py check_df_for_duplicates --notify 1
-./tele2_metrics.py check_if_data_in_table
+./tele2_metrics.py compare_new_df_with_retro --notify 1 --model_id 135 --stage_db 'hadoop' \
+--stage_table 'developers.eg_msg_traf_1' --final_db 'teradata' --final_table 'prd2_dds_v.scoring' \
+--retro_type 'retro_df_many_dates' --threshold 15 --retro_date '2021-03-04' --report_date '2021-06-04' \
+--send_to 'ekaterina.gruzdova@tele2.ru'
+if [ $? -eq 0 ]; then
+  ./tele2_metrics.py check_df_for_duplicates --notify 1 --model_id 135 --stage_db 'hadoop' \
+  --stage_table 'developers.eg_msg_traf_1' --column 'subs_id' --send_to 'ekaterina.gruzdova@tele2.ru'
+else
+  echo 'FAIL'
+  exit 1
+fi
+
+./tele2_metrics.py check_if_data_in_table --model_id 135 --final_db 'teradata' \
+--final_table 'prd2_dds_v.scoring' --report_date '2021-06-04' --send_to 'ekaterina.gruzdova@tele2.ru' --notify 1
