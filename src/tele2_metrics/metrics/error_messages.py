fabd67e483d284b7bba8ef0b70d9efd236c82ed6
ekaterina.gruzdova <ekaterina.gruzdova@tele2.ru>
2021-06-24 01:11:58 +0300

    edit error messages

--- src/tele2_metrics/metrics/error_messages.py
+++ src/tele2_metrics/metrics/error_messages.py
@@ -1,8 +1,8 @@
 from dynaconf import settings
 
-error_text_duplicates = f"""В результатах скоринга модели (таблица {settings.METRICS_STAGE_TABLE}) содержатся дубли. 
+error_text_duplicates = f"""В результатах скоринга модели (таблица {settings.METRICS_STAGE_TABLE}) содержатся дубли за report_date {settings.METRICS_REPORT_DATE}. 
 Скоринг остановлен. Проверьте модель"""
-error_text_compare_with_retro = """Обнаружено падение/увеличение объема результатов скоринга модели. 
+error_text_compare_with_retro = f"""Обнаружено аномальное количество скоров в результатах модели за report_date {settings.METRICS_REPORT_DATE}. 
 Скоринг остановлен. Проверьте модель"""
-error_text_no_data = f"""Модель отработала, но результаты не были доставлены в {settings.METRICS_FINAL_TABLE}.
+error_text_no_data = f"""Модель отработала, но результаты не были доставлены в {settings.METRICS_FINAL_TABLE} за report_date {settings.METRICS_REPORT_DATE}.
 Проверьте модель"""
