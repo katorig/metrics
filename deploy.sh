3e5d0fba254c3175c0e39999ff1da3d3258435f9
Gruzdova Ekaterina <ekaterina.gruzdova@tele2.ru>
2021-09-16 12:27:53 +0000

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -2,8 +2,8 @@ pip install twine --timeout 10 --index-url http://nexus.t2ru-bda-wds-008.corp.te
 --trusted-host nexus.t2ru-bda-wds-008.corp.tele2.ru
 python3 setup.py clean --all
 python3 setup.py bdist_wheel
-#twine upload dist/* --verbose --repository-url=http://nexus.t2ru-bda-wds-008.corp.tele2.ru/repository/pypi-hosted/ \
-#-u ${NEXUS_USER} -p ${NEXUS_PASSWORD}
+twine upload dist/* --verbose --repository-url=http://nexus.t2ru-bda-wds-008.corp.tele2.ru/repository/pypi-hosted/ \
+-u ${NEXUS_USER} -p ${NEXUS_PASSWORD}
 
-#twine upload dist/* --verbose --repository-url=https://artifactory.tdp.corp.tele2.ru/artifactory/api/pypi/bdo-pypi \
-#-u ${FROG_USER} -p ${FROG_PASSWORD}
+twine upload dist/* --verbose --repository-url=https://artifactory.tdp.corp.tele2.ru/artifactory/api/pypi/bdo-pypi \
+-u ${FROG_USER} -p ${FROG_PASSWORD}
